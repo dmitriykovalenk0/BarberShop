f7bb737b19363904bb9924ff294cce63fd7c54b8
dmitriykovalenk0 <dimakovalenko1999god@gmail.com>
2020-03-15 18:36:20 +0200

    create header-slider

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+  $('.header__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<img class="nextArrow slider-nextArrow" src="../img/icons/slider-arrow.png" alt="arrow-right">',
+    prevArrow: '<img class="prevArrow slider-prevArrow" src="../img/icons/slider-arrow.png" alt="arrow-left">'
+  });
+});
