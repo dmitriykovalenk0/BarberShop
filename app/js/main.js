58a7fbb11c762e8708d84399ef4e5950b358096c
dmitriykovalenk0 <dimakovalenko1999god@gmail.com>
2020-03-16 16:50:02 +0200

    create reviews section

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,12 @@ $(document).ready(function(){
     nextArrow: '<img class="nextArrow slider-nextArrow" src="../img/icons/slider-arrow.png" alt="arrow-right">',
     prevArrow: '<img class="prevArrow slider-prevArrow" src="../img/icons/slider-arrow.png" alt="arrow-left">'
   });
+
+  $('.reviews__slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: '<img class="nextArrow slider-nextArrow" src="../img/icons/slider-arrow.png" alt="arrow-right">',
+    prevArrow: '<img class="prevArrow slider-prevArrow" src="../img/icons/slider-arrow.png" alt="arrow-left">'
+  });
 });
